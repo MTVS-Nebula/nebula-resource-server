1a6a09246496f79f3f31ad7f1614ce4ee37ed6ab
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-31 10:15:23 +0900

    initiate: 스프링 부트 초기 설정

---
+++ src/main/java/com/nebula/nebula_resource/NebulaResourceApplication.java
@@ -0,0 +1,13 @@
+package com.nebula.nebula_resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NebulaResourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nebula/nebula_resource/NebulaResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebula.nebula_resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
