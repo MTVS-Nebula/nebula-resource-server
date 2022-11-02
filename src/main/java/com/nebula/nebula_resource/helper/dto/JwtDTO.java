f952d9800b3f21f2b477bf99f02d6e21e35a49d3
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-02 15:43:42 +0900

    feature(Jwt): JWT 데이터분석을 위한 DTO 제작

---
+++ src/main/java/com/nebula/nebula_resource/helper/dto/JwtDTO.java
@@ -0,0 +1,51 @@
+package com.nebula.nebula_resource.helper.dto;
+
+import java.util.List;
+
+public class JwtDTO {
+    private int id;
+    private String username;
+    private List<String> roles;
+
+    public JwtDTO() {
+    }
+
+    public JwtDTO(int id, String username, List<String> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "JwtDTO{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", role=" + roles +
+                '}';
+    }
+}
