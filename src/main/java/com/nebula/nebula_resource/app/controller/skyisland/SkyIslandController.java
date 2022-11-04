e2941c3db9965632f1dcf06fd21571f1e9e5bac8
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-04 14:49:38 +0900

    feature(skyisland): 하늘섬 정보 로드를 위한 컨트롤러 제작 #14

--- src/main/java/com/nebula/nebula_resource/app/controller/skyisland/SkyIslandController.java
+++ src/main/java/com/nebula/nebula_resource/app/controller/skyisland/SkyIslandController.java
@@ -1,10 +1,19 @@
 package com.nebula.nebula_resource.app.controller.skyisland;
 
+import com.nebula.nebula_resource.app.dto.skyisland.IslandPlaceObjectDTO;
+import com.nebula.nebula_resource.app.dto.skyisland.SkyIslandDTO;
 import com.nebula.nebula_resource.app.service.skyisland.SkyIslandService;
+import com.nebula.nebula_resource.helper.api.ResponseMessage;
+import com.nebula.nebula_resource.helper.api.ResultResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("skyisland")
@@ -14,5 +23,19 @@ public class SkyIslandController {
     @Autowired
     public SkyIslandController(SkyIslandService skyIslandService) {
         this.skyIslandService = skyIslandService;
+    }
+
+    @GetMapping("{islandId}")
+    public ResponseEntity<?> getSkyIslandObjects(@PathVariable int islandId){
+        try {
+            SkyIslandDTO result = skyIslandService.loadSkyIslandById(islandId);
+            return ResponseEntity
+                    .ok()
+                    .body(new ResultResponseMessage(HttpStatus.OK.value(), "success", result));
+        } catch (RuntimeException e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new ResponseMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+        }
     }
 }
