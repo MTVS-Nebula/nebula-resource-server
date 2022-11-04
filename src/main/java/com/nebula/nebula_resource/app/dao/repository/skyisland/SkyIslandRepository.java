1e827a4972bd3be994e7cff32c12e6f7de9dc8f7
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-04 15:03:51 +0900

    fix(skyisland): Jpa repository template의 오류 수정 #14

--- src/main/java/com/nebula/nebula_resource/app/dao/repository/skyisland/SkyIslandRepository.java
+++ src/main/java/com/nebula/nebula_resource/app/dao/repository/skyisland/SkyIslandRepository.java
@@ -3,6 +3,6 @@ package com.nebula.nebula_resource.app.dao.repository.skyisland;
 import com.nebula.nebula_resource.app.dao.entity.skyisland.SkyIsland;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SkyIslandRepository extends JpaRepository<Integer, SkyIsland> {
+public interface SkyIslandRepository extends JpaRepository<SkyIsland, Integer> {
     public SkyIsland findById(int id);
 }
