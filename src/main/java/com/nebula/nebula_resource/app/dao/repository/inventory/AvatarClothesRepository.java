493227d7912209059200eb74e00b87b88886b588
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-09 19:20:32 +0900

    feature(clothes) : 착용 장비 정보를 로드/세이브 할수 있도록 repository 구현 #26
    구현 내용
    - 아바타 이름으로 아바타-옷 관계 삭제
    - 아바타 이름으로 장비 정보 조회

--- src/main/java/com/nebula/nebula_resource/app/dao/repository/inventory/AvatarClothesRepository.java
+++ src/main/java/com/nebula/nebula_resource/app/dao/repository/inventory/AvatarClothesRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AvatarClothesRepository extends JpaRepository<AvatarClothes, Integer> {
     List<AvatarClothes> findByAvatarAvatarName(String avatarName);
+    int deleteByAvatarAvatarName(String avatarName);
 }
---
+++ src/main/java/com/nebula/nebula_resource/app/dao/repository/inventory/AvatarEquipmentRepository.java
@@ -0,0 +1,10 @@
+package com.nebula.nebula_resource.app.dao.repository.inventory;
+
+import com.nebula.nebula_resource.app.dao.entity.inventory.AvatarEquipment;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvatarEquipmentRepository extends JpaRepository<AvatarEquipment, Integer> {
+    List<AvatarEquipment> findByAvatarAvatarName(String avatarName);
+    int deleteByAvatarAvatarName(String avatarName);
+}
