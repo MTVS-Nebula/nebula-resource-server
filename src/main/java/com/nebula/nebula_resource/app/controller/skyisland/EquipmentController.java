8ee2f9f2bad822ee897ebc27a104f5dbc5b5197e
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-09 19:21:46 +0900

    feature(clothes) : 착용 장비 정보를 로드/세이브 기능 컨트롤러 구현 #26

---
+++ src/main/java/com/nebula/nebula_resource/app/controller/skyisland/EquipmentController.java
@@ -0,0 +1,54 @@
+package com.nebula.nebula_resource.app.controller.skyisland;
+
+import com.nebula.nebula_resource.app.dto.inventory.InventoryDTO;
+import com.nebula.nebula_resource.app.dto.inventory.InventoryEquipmentDTO;
+import com.nebula.nebula_resource.app.service.inventory.EquipmentService;
+import com.nebula.nebula_resource.helper.api.ResponseMessage;
+import com.nebula.nebula_resource.helper.api.ResultResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("equipment")
+public class EquipmentController {
+    private final EquipmentService equipmentService;
+    @Autowired
+    public EquipmentController(EquipmentService equipmentService) {
+        this.equipmentService = equipmentService;
+    }
+
+    @GetMapping("{avatarName}")
+    public ResponseEntity<?> saveEquipment(@PathVariable String avatarName){
+        try {
+            InventoryDTO result = equipmentService.getEquipment(avatarName);
+            return ResponseEntity
+                    .ok()
+                    .body(new ResultResponseMessage(HttpStatus.CREATED.value(), "success",result));
+        } catch (RuntimeException e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new ResponseMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+        }
+    }
+
+    @PostMapping("{avatarName}")
+    public ResponseEntity<?> saveEquipment(@PathVariable String avatarName, @RequestBody InventoryEquipmentDTO inventoryEquipmentDTO){
+        try {
+            equipmentService.saveEquipment(avatarName, inventoryEquipmentDTO);
+            return ResponseEntity
+                    .ok()
+                    .body(new ResponseMessage(HttpStatus.CREATED.value(), "success"));
+        } catch (RuntimeException e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new ResponseMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+        }
+    }
+}
