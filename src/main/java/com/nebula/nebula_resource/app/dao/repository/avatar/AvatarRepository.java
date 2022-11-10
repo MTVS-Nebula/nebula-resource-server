81d1f6869c82a9a6dd761f104667d867357dda51
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-10 15:46:46 +0900

    feature(avatar) : Avatar 를 계정명으로 찾을 수 있는 메소드 추가 #8

--- src/main/java/com/nebula/nebula_resource/app/dao/repository/avatar/AvatarRepository.java
+++ src/main/java/com/nebula/nebula_resource/app/dao/repository/avatar/AvatarRepository.java
@@ -1,8 +1,10 @@
 package com.nebula.nebula_resource.app.dao.repository.avatar;
 
 import com.nebula.nebula_resource.app.dao.entity.avatar.Avatar;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AvatarRepository extends JpaRepository<Avatar, Integer> {
     Avatar findByAvatarName(String avatarName);
+    List<Avatar> findByOwnerUsername(String userName);
 }
