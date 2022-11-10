69d867d06d0c8a6800ea2b911e6b209a1be2cddb
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-10 17:15:49 +0900

    feature(avatar) : 아바타 생성시 아바타 수 확인을 위한 메소드 구현 #8

--- src/main/java/com/nebula/nebula_resource/app/dao/repository/avatar/AvatarRepository.java
+++ src/main/java/com/nebula/nebula_resource/app/dao/repository/avatar/AvatarRepository.java
@@ -1,10 +1,12 @@
 package com.nebula.nebula_resource.app.dao.repository.avatar;
 
 import com.nebula.nebula_resource.app.dao.entity.avatar.Avatar;
+import com.nebula.nebula_resource.app.dao.entity.user.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AvatarRepository extends JpaRepository<Avatar, Integer> {
     Avatar findByAvatarName(String avatarName);
     List<Avatar> findByOwnerUsername(String userName);
+    int countByOwnerAndIsDeleted(User user, String isDeleted);
 }
