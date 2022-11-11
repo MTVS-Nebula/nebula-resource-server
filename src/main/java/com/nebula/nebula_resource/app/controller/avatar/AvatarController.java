a506513b501dd3b86c70a40367b0aa0cc6be2662
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-11 09:42:28 +0900

    feature(avatar) : 아바타 텍스처 정보 저장을 위한 컨트롤러 구현 #8

--- src/main/java/com/nebula/nebula_resource/app/controller/avatar/AvatarController.java
+++ src/main/java/com/nebula/nebula_resource/app/controller/avatar/AvatarController.java
@@ -7,11 +7,14 @@ import com.nebula.nebula_resource.helper.api.ResponseMessage;
 import com.nebula.nebula_resource.helper.api.ResultResponseMessage;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -43,11 +46,38 @@ public class AvatarController {
     @PostMapping
     public ResponseEntity<?> createAvatar(AvatarCreateDTO createInfo){
         try {
-            System.out.println(createInfo);
             avatarService.createAvatar(createInfo);
             return ResponseEntity
                     .created(URI.create("/avatar"))
                     .body(new ResultResponseMessage(HttpStatus.CREATED.value(), "success",createInfo.getAvatarName()));
+        } catch (RuntimeException e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new ResponseMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+        }
+    }
+
+    @GetMapping("duplication/{avatarName}")
+    public ResponseEntity<?> checkAvatarNameDuplicated(@PathVariable String avatarName){
+        try {
+
+            return ResponseEntity
+                    .created(URI.create("/avatar/texture"))
+                    .body(new ResponseMessage(HttpStatus.CREATED.value(), "available"));
+        } catch (RuntimeException e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new ResponseMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+        }
+    }
+
+    @PostMapping("texture/{avatarName}")
+    public ResponseEntity<?> saveAvatarTexture(@PathVariable String avatarName, @RequestBody Map<String , Object> textureData){
+        try {
+            avatarService.saveTexture(avatarName, textureData);
+            return ResponseEntity
+                    .created(URI.create("/avatar/texture"))
+                    .body(new ResponseMessage(HttpStatus.CREATED.value(), "success"));
         } catch (RuntimeException e){
             return ResponseEntity
                     .badRequest()
