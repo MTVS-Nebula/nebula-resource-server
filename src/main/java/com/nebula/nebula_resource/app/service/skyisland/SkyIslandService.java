4a70818d4e3c22a4f74d1186af049b439dcdb22e
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-11 15:03:28 +0900

    feature(skyisland) : 좌표를 저장하는 서비스로직 구현 #36

--- src/main/java/com/nebula/nebula_resource/app/service/skyisland/SkyIslandService.java
+++ src/main/java/com/nebula/nebula_resource/app/service/skyisland/SkyIslandService.java
@@ -1,11 +1,14 @@
 package com.nebula.nebula_resource.app.service.skyisland;
 
 import com.nebula.nebula_resource.app.dto.skyisland.IslandPlaceObjectDTO;
+import com.nebula.nebula_resource.app.dto.skyisland.SkyIslandCoordinateDTO;
 import com.nebula.nebula_resource.app.dto.skyisland.SkyIslandDTO;
+import java.util.Map;
 import org.springframework.stereotype.Service;
 
 public interface SkyIslandService {
     public void createIsland(int avatarId);
     public SkyIslandDTO loadSkyIslandById(int skyIslandId);
     public void saveSkyIslandById(int skyIsland, IslandPlaceObjectDTO islandPlaceObjectDTO);
+    public Map<Integer, SkyIslandCoordinateDTO> getSkyIslandMap();
 }
