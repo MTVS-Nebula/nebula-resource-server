5c049ee5b03bd9b8d746a52aec92ea7fc487c0fc
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-17 14:54:26 +0900

    fix(achieve) : 아이템 검증 로직에 장비 아이템도 추가 #49

--- src/main/java/com/nebula/nebula_resource/app/dao/repository/inventory/AvatarEquipmentRepository.java
+++ src/main/java/com/nebula/nebula_resource/app/dao/repository/inventory/AvatarEquipmentRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AvatarEquipmentRepository extends JpaRepository<AvatarEquipment, Integer> {
     List<AvatarEquipment> findByAvatarAvatarName(String avatarName);
     int deleteByAvatarAvatarName(String avatarName);
+    int countByClothesId(int uniqueId);
 }
