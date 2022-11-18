e8f587e7f9b3712361fca0f18889787532d35c52
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-18 22:38:13 +0900

    refactor(inventory) : Transactional 태그 추가 #49

--- src/main/java/com/nebula/nebula_resource/app/service/inventory/impl/AchieveItemServiceImpl.java
+++ src/main/java/com/nebula/nebula_resource/app/service/inventory/impl/AchieveItemServiceImpl.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AchieveItemServiceImpl implements AchieveItemService {
@@ -31,6 +32,7 @@ public class AchieveItemServiceImpl implements AchieveItemService {
     }
 
     @Override
+    @Transactional
     public void pickUpClothes(String avatarName, int clothesUniqueId) {
         Avatar avatar = avatarRepository.findByAvatarName(avatarName);
         PermissionChecker.checkAvatarPermission(avatar);
