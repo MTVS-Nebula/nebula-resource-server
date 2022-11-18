e073c8742a2e178df723c1ba44326ff10735e10c
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-18 22:36:05 +0900

    feature(inventory) : 아바타로 옷 인벤토리 조회 메소드 구현 #49

--- src/main/java/com/nebula/nebula_resource/app/dao/repository/inventory/AvatarClothesRepository.java
+++ src/main/java/com/nebula/nebula_resource/app/dao/repository/inventory/AvatarClothesRepository.java
@@ -1,11 +1,13 @@
 package com.nebula.nebula_resource.app.dao.repository.inventory;
 
+import com.nebula.nebula_resource.app.dao.entity.avatar.Avatar;
 import com.nebula.nebula_resource.app.dao.entity.inventory.AvatarClothes;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AvatarClothesRepository extends JpaRepository<AvatarClothes, Integer> {
     List<AvatarClothes> findByAvatarAvatarName(String avatarName);
+    List<AvatarClothes> findByAvatar(Avatar avatar);
     int deleteByAvatarAvatarName(String avatarName);
     int countByClothesId(int uniqueId);
 }
