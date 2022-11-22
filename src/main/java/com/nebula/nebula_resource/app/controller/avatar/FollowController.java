6129e60148de91d362c8da7c647da15e3cf3d55f
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-22 16:36:29 +0900

    feature(follow) : 팔로우 리스트 조회(getFollowList) 컨트롤러 로직 제작 #60

--- src/main/java/com/nebula/nebula_resource/app/controller/avatar/FollowController.java
+++ src/main/java/com/nebula/nebula_resource/app/controller/avatar/FollowController.java
@@ -1,14 +1,17 @@
 package com.nebula.nebula_resource.app.controller.avatar;
 
 import com.nebula.nebula_resource.app.dto.avatar.FollowRequestDTO;
+import com.nebula.nebula_resource.app.dto.avatar.FollowResponseVO;
 import com.nebula.nebula_resource.app.service.avatar.FollowService;
 import com.nebula.nebula_resource.helper.api.ResponseMessage;
+import com.nebula.nebula_resource.helper.api.ResultResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("follow")
@@ -42,6 +45,20 @@ public class FollowController {
             return ResponseEntity
                     .ok()
                     .body(new ResponseMessage(HttpStatus.OK.value(), "success"));
+        } catch (RuntimeException e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new ResponseMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+        }
+    }
+
+    @GetMapping("{avatarName}")
+    public ResponseEntity<?> getFollowList(@PathVariable String avatarName){
+        try {
+            List<FollowResponseVO> result = followService.getAvatarFollowingList(avatarName);
+            return ResponseEntity
+                    .ok()
+                    .body(new ResultResponseMessage(HttpStatus.OK.value(), "success", result));
         } catch (RuntimeException e){
             return ResponseEntity
                     .badRequest()
