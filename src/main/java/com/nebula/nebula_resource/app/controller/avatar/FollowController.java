ab96301381faab05325715ec0bb6453fc0ada50a
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-22 16:05:04 +0900

    feature(follow) : 신규 Follow 컨트롤러 로직 구현 #60

---
+++ src/main/java/com/nebula/nebula_resource/app/controller/avatar/FollowController.java
@@ -0,0 +1,36 @@
+package com.nebula.nebula_resource.app.controller.avatar;
+
+import com.nebula.nebula_resource.app.dto.avatar.FollowRequestDTO;
+import com.nebula.nebula_resource.app.service.avatar.FollowService;
+import com.nebula.nebula_resource.helper.api.ResponseMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("follow")
+public class FollowController {
+    private final FollowService followService;
+    @Autowired
+    public FollowController(FollowService followService) {
+        this.followService = followService;
+    }
+
+    @PostMapping("{avatarName}")
+    public ResponseEntity<?> createAvatar(@PathVariable String avatarName,
+                                          @RequestBody FollowRequestDTO followRequestDTO){
+        try {
+            followService.followAvatar(avatarName, followRequestDTO.getFollowingName());
+            return ResponseEntity
+                    .created(URI.create("/follow"))
+                    .body(new ResponseMessage(HttpStatus.CREATED.value(), "success"));
+        } catch (RuntimeException e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new ResponseMessage(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+        }
+    }
+}
