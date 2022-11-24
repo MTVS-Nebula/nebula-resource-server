0fc6a295787a621373a56090da4a3d378d11be44
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-24 13:18:12 +0900

    feature(purchase) : 구매 가능 리스트를 확인하기 위한 서비스로직 구현 #71
    구현 목록
    - getPurchaseList
    - convertBuildingBundleListToDTO
    - getPurchasableBuildingBundleList
    - getOwnedBundleIdSet
    - convertClothesRotationToDTO

--- src/main/java/com/nebula/nebula_resource/app/service/purchase/PurchaseService.java
+++ src/main/java/com/nebula/nebula_resource/app/service/purchase/PurchaseService.java
@@ -1,7 +1,10 @@
 package com.nebula.nebula_resource.app.service.purchase;
 
 import com.nebula.nebula_resource.app.dto.purchase.PurchaseBuildingBundleDTO;
+import com.nebula.nebula_resource.app.dto.purchase.PurchaseListDTO;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PurchaseService {
+    PurchaseListDTO getPurchaseList(String avatarName);
     PurchaseBuildingBundleDTO purchaseBundle(String avatarName, String bundleName);
 }
