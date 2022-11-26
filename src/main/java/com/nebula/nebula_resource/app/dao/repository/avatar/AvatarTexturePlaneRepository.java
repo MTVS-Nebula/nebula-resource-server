8b52e96ea0a8ecbc06b23bb73a07c1eea089cd23
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-27 03:28:53 +0900

    feature(texture) : 아이디로 텍스처를 찾는 메소드 추가 #78

--- src/main/java/com/nebula/nebula_resource/app/dao/repository/avatar/AvatarTexturePlaneRepository.java
+++ src/main/java/com/nebula/nebula_resource/app/dao/repository/avatar/AvatarTexturePlaneRepository.java
@@ -4,5 +4,6 @@ import com.nebula.nebula_resource.app.dao.entity.avatar.AvatarTexturePlane;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AvatarTexturePlaneRepository extends JpaRepository<AvatarTexturePlane, Integer> {
-
+    AvatarTexturePlane findById(int id);
+    int deleteByAvatarId(int avatarId);
 }
