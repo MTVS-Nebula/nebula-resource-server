ebacb9540d8c5b2fe7c6f78b16eb703546c9e241
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-12-13 21:06:07 +0900

    feature(create-avatar) : 어플리케이션이 비동기 호출을 사용할 수 있도록 @EnableAsync 추가 #42

--- src/main/java/com/nebula/nebula_resource/NebulaResourceApplication.java
+++ src/main/java/com/nebula/nebula_resource/NebulaResourceApplication.java
@@ -2,8 +2,10 @@ package com.nebula.nebula_resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class NebulaResourceApplication {
 
     public static void main(String[] args) {
