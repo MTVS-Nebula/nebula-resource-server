565dd2e2717fb5cfb6379efe1d24c012cc9fa54f
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-12-13 21:05:46 +0900

    feature(create-avatar) : 맵 생성시 API호출 서비스 구현  #42

---
+++ src/main/java/com/nebula/nebula_resource/helper/callapi/CallRefreshMapApiService.java
@@ -0,0 +1,26 @@
+package com.nebula.nebula_resource.helper.callapi;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class CallRefreshMapApiService {
+    @Async
+    public void sendRefreshMapRequest(){
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "http://ai-alb-1793409698.ap-northeast-2.elb.amazonaws.com";
+        HttpHeaders header = new HttpHeaders();
+        HttpEntity<?> entity = new HttpEntity<>(header);
+
+        UriComponents uri = UriComponentsBuilder.fromHttpUrl(url).build();
+        restTemplate.exchange(uri.toString(), HttpMethod.GET, entity, Object.class);
+    }
+}
